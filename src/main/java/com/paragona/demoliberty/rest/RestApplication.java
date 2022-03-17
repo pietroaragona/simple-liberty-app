0622e46daba6349876e47dc5018d618732bf921f
Pietro Aragona <paragona@sogei.it>
2022-03-17 12:09:13 +0100

    initial commit

---
+++ src/main/java/com/paragona/demoliberty/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.paragona.demoliberty.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
